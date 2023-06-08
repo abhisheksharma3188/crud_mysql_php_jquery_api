56c10c8a33a07300b5ee32f82d7d7c1e8de8c13b
abhisheksharma3188 <111152006+abhisheksharma3188@users.noreply.github.com>
2023-06-09 01:03:51 +0530

    Create connection.php

---
+++ api/connection.php
@@ -0,0 +1,7 @@
+<?php
+$hostname='localhost';
+$username='YOUR_USERNAME';
+$password='YOUR_PASSWORD';
+$database='YOUR_DATABASE_NAME';
+$connection=@mysqli_connect($hostname,$username,$password,$database);
+?>
